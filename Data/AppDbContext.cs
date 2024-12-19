c36ff9e1065bbcd89472135b322444b327ce50a4
Rachana <kunapareddyrachana@gmail.com>
2024-12-19 00:06:19 -0500

    explain dbcontext

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -6,6 +6,10 @@ namespace eTickets.Data
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         //AppDbContext = constructor
+        //DbContextOptions = Parameter
+        //AppDbContext = Parameter Filename
+        //options = Parameter name options
+        //base = pass the parameter options to baseclass using base keyword
         {
 
         }
